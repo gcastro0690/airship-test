e404c98d91790c294b5b5821db7f27c4029e979a
Gonzalo Castro <gcastro@Gonzalos-MacBook-Pro.local>
2022-07-28 18:16:53 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   useEffect(() => {
     const anyWindow: any = window;
     anyWindow.UA.then((sdk: any) => {
+      debugger;
       sdk.register();
       sdk.getChannel().then((channel: any) => {
         debugger;
